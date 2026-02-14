1db2108f596ce9f7d72e4926036ca12acf164674
Gryphon998 <cyosun1129@gmail.com>
2026-02-14 00:45:59 -0500

    Project initiated.

---
+++ src/main/java/com/platform/campusrecruitmentportal/CampusRecruitmentPortalApplication.java
@@ -0,0 +1,13 @@
+package com.platform.campusrecruitmentportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusRecruitmentPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusRecruitmentPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/platform/campusrecruitmentportal/CampusRecruitmentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.campusrecruitmentportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusRecruitmentPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
